7bf33cdce7577c8f3711bdcab535475b119fa921
atfcreative <atfcreative@gmail.com>
2018-11-20 11:05:36 -0800

    adds sanity check to back end --working on localhost:4000

--- server/server.js
+++ server/server.js
@@ -1,15 +1,60 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-app.use()
-app.use()
+// require db//
+// const db = require('./models/index');
+// const Post = db.Post;
+
+//====================================================
+///////MIDDLE-WARE////////////////////////////////////
+//====================================================
+
+//Require use of Express CORS middle ware//
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//Require Express Body Parser middle ware//
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
-app.get('/', req, res) => {
-    res.send(<h1>Saludos, saludos amigos!</h1>);
-    console.log('Hello World');
-}
+//====================================================
+///////REQUIRE API////////////////////////////////////
+//====================================================
 
+//Users//
+// const userRouter = require('./config/api/users/routes');
+
+// //Sched Tour//
+// const tourRouter = require('./config/api/tours/routes');
+
+// //Plan type//
+// const planRouter = require('./config/api/plans/routes');
+
+//====================================================
+///////API-ENDPOINTS-ROUTES//////////////////////////////////
+//====================================================
+// app.use('/api/users', userRouter);
+// app.use('api/tours', tourRouter);
+// app.use('api/plans', planRouter);
+
+
+//====================================================
+///////SANITY CHECK//////////////////////////////////
+//====================================================
+app.get('/', (req, res) => {
+    res.send('<h1>&#128372; Moonwalking Micheal Jackson style...</h1>');
+});
+
+
+
+//====================================================
+///////START SERVER///////////////////////////////////
+//====================================================
 app.listen(port, () => console.log(`Server moonwalking like Mike J. on port ${port}`));
 
