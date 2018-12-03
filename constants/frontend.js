c5895ab7140b0d7e045eabe8b9ef25241de9aa0d
atfcreative <atfcreative@gmail.com>
2018-12-03 14:28:46 -0800

    fixes url typo

--- constants/frontend.js
+++ constants/frontend.js
@@ -1,8 +1,8 @@
 const FRONTEND_DEV_URLS = [ 'http://localhost:3000' ];
 
 const FRONTEND_PROD_URLS = [ 
-    'htpps://www.mydomain.com',
-    'htpps://mydomain.com'
+    'https://www.mydomain.com',
+    'https://mydomain.com'
  ];
 
  module.exports = process.env.NODE_ENV === 'production'
